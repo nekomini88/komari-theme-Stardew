a4d02efd02e3631922bad99edf45173a0724afce
Nekomini <_EMAIL>
2026-07-31 14:34:17 +0800

    feat: add Silkscreen pixel font import and PixelFontLink loader

---
+++ src/components/PixelFontLink.vue
@@ -0,0 +1,8 @@
+<template>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Silkscreen&display=swap" rel="stylesheet" />
+</template>
+
+<script setup lang="ts">
+</script>
